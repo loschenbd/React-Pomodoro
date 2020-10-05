521a28f0b7be0f1d35abc6e80b9112326587f014
Benjamin Loschen <loschenbd@gmail.com>
2020-10-05 14:43:40 -0500

    Updates

---
+++ precache-manifest.d9af5f101fe149726e1c7b2753f13b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40862557557a2186946168249e829e7d",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "af5c4d2f99c35646bc50",
+    "url": "/react-pomodoro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dab0882193d0310971ba",
+    "url": "/react-pomodoro/static/js/2.1a5dc55e.chunk.js"
+  },
+  {
+    "revision": "f5eeca88c65df516af9519a8bb5412f5",
+    "url": "/react-pomodoro/static/js/2.1a5dc55e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5c4d2f99c35646bc50",
+    "url": "/react-pomodoro/static/js/main.9fa54c16.chunk.js"
+  },
+  {
+    "revision": "7bccaee7c4903395c012",
+    "url": "/react-pomodoro/static/js/runtime-main.44319084.js"
+  }
+]);
