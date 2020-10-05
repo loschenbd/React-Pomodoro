cfcd9a6c4d06620f600db9462f9d44e67b7cbd30
Benjamin Loschen <loschenbd@gmail.com>
2020-10-05 11:25:06 -0500

    Added Momentjs and FCC tests. Wrote to-do

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*
+      - Add Session Timer
+      - Add break Timer
+      - Add Timer Display
+        + change TimeDisplay whenever session length changes (useEffect - called whenever a variable changes)
+        + Add countdown ( setInterval() )
+        + Stop Countdown ( clearInterval() ), intervalId -
+      - Add time complete alert  - Add audio alert and play it (useRef)
+      - Switch from Session to Break and visa versa (track session type) - set time left to break/session length
+      - Add Reset Button
+      */}
     </div>
   );
 }
