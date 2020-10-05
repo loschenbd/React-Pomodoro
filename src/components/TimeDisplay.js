3b1ba69bd306d63cf2802803dc0c5c1b33b15e38
Benjamin Loschen <loschenbd@gmail.com>
2020-10-05 12:34:28 -0500

    Added countdown.

--- src/components/TimeDisplay.js
+++ src/components/TimeDisplay.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format';
 
@@ -6,11 +6,27 @@ momentDurationFormatSetup(moment);
 
 const TimeDisplay = ({sessionLength}) => {
   const [timeLeft, setTimeLeft] = useState(sessionLength);
+  useEffect(() => {
+    setTimeLeft(sessionLength);
+  }, [sessionLength]);
 
   const timeLabel = moment.duration(timeLeft, 's').format('mm:ss');
+
+  const startStopTime = () => {
+    setInterval(() => {
+      setTimeLeft(prevTimeLeft => {
+        const newTimeLeft = prevTimeLeft - 1;
+        if (newTimeLeft >= 0) {
+          return prevTimeLeft - 1
+        }
+        return prevTimeLeft
+      })
+    }, 1000)
+  };
   return (
     <div>
       {timeLabel}
+      <button onClick={startStopTime}>start</button>
     </div>
   );
 };
